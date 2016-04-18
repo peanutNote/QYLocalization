63d2a4e061d8e4da8bfa552d98c52721dc9222fa
yixiong <yixiong@chemanman.com>
2016-04-18 16:55:36 +0800

    Initial Commit

---
+++ QYLocalization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYLocalization
+//
+//  Created by qianye on 16/4/18.
+//  Copyright © 2016年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYLocalization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYLocalization
+//
+//  Created by qianye on 16/4/18.
+//  Copyright © 2016年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
